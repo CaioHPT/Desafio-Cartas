ba32231785bd005fb07bc79f4efd2483636d7bce
CaioHPT <caio.h.teixeira@gmail.com>
2022-06-23 15:17:52 -0300

    criação das pastas

---
+++ backend/klabDesafio/src/test/java/br/com/caio/klabDesafio/KlabDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caio.klabDesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlabDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
