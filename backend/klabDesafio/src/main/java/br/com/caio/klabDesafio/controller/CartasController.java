881eaefd71f8a910149765c37026812b48ac29fe
CaioHPT <caio.h.teixeira@gmail.com>
2022-06-23 20:38:44 -0300

    add cross origin

--- backend/klabDesafio/src/main/java/br/com/caio/klabDesafio/controller/CartasController.java
+++ backend/klabDesafio/src/main/java/br/com/caio/klabDesafio/controller/CartasController.java
@@ -1,5 +1,6 @@
 package br.com.caio.klabDesafio.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/deck")
+@CrossOrigin(origins = "*")
 public class CartasController {
 	
 	@GetMapping
