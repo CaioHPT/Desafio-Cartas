df576e03e3e17cb6822d2045edc36f5163ef8ce6
CaioHPT <caio.h.teixeira@gmail.com>
2022-06-23 22:18:25 -0300

    estilizando os cards e as imagens e criação dos itens no html com js

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -10,31 +10,75 @@ interface Card {
 }
 
 
-async function getCards(deck_id: string, qtd: number){
+async function getCards(deck_id: string, qtd: number) {
     const response = await fetch(`http://localhost:8080/deck/cartas/${deck_id}/${qtd}`)
     const data = await response.json()
 
     constroi(data.cards)
 }
 
-async function getDeck(){
+async function getDeck() {
     const response = await fetch("http://localhost:8080/deck")
     const data = await response.json()
 
-    getCards(data.deck_id, 2)
+    getCards(data.deck_id, 20)
 }
 
 getDeck()
 
-function constroi(cards: Card[]){
-    console.log(cards)
+function constroi(cards: Card[]) {
+    const texto = document.createElement("p")
+    texto.textContent = "Jogador 1"
+
     const main = document.querySelector("main")
-    cards.forEach(card => {
-        const div = document.createElement("div")
-        const img = document.createElement("img")
-        img.src = card.image
-        div.appendChild(img)
-        main?.appendChild(div)
-    })
+
+    let cont: number = 0
+    let maiorSoma : number = 0
+    let jogadorVencedor : string = '' 
+
+    for (let i = 0; i < 4; i++) {
+        let soma : number = 0
+        const divCards = document.createElement("div")
+        
+        divCards.setAttribute('class', "divCard")
+
+        const cardItem = document.createElement("div")
+
+        cardItem.setAttribute('class', 'cards')
+        divCards.appendChild(texto)
+        divCards.appendChild(cardItem)
+        
+        for (let j = 0; j < 5; j++) {
+            console.log(cards[cont].value)
+            switch(cards[cont].value){
+                case "ACE":
+                    soma += 1
+                    break
+                case "JACK":
+                    soma += 11
+                    break
+                case "KING":
+                    soma += 13
+                    break
+                case "QUEEN":
+                    soma += 12
+                    break
+                default:
+                    soma += Number(cards[cont].value)
+            }
+            const img = document.createElement("img")
+            img.src = cards[cont].image
+            cardItem.appendChild(img)
+            cont++
+        }
+
+        if(soma > maiorSoma){
+            maiorSoma = soma
+            jogadorVencedor = `jogador ${i + 1}`
+        }
+
+        main?.appendChild(divCards)
+    }
+
 }
 
