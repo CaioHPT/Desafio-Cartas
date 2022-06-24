2e5500664da102a1d9811d455da2407ef972bf30
CaioHPT <caio.h.teixeira@gmail.com>
2022-06-23 22:34:16 -0300

    ajuste na responsividade

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,13 +1,26 @@
 "use strict";
-async function getCards(deck_id, qtd) {
-    const response = await fetch(`http://localhost:8080/deck/cartas/${deck_id}/${qtd}`);
-    const data = await response.json();
-    constroi(data.cards);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function getCards(deck_id, qtd) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch(`http://localhost:8080/deck/cartas/${deck_id}/${qtd}`);
+        const data = yield response.json();
+        constroi(data.cards);
+    });
 }
-async function getDeck() {
-    const response = await fetch("http://localhost:8080/deck");
-    const data = await response.json();
-    getCards(data.deck_id, 20);
+function getDeck() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("http://localhost:8080/deck");
+        const data = yield response.json();
+        getCards(data.deck_id, 20);
+    });
 }
 getDeck();
 function constroi(cards) {
@@ -52,6 +65,6 @@ function constroi(cards) {
             maiorSoma = soma;
             jogadorVencedor = `jogador ${i + 1}`;
         }
-        main?.appendChild(divCards);
+        main === null || main === void 0 ? void 0 : main.appendChild(divCards);
     }
 }
